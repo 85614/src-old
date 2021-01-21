ce3ad76b48d206c5a1fe1cfdb5840b39006b0f98
ZhangZhiQiang <2510406196@qq.com>
2021-01-21 10:04:00 +0800

    Test make_kernel_name

--- operator/opencl-tool.h
+++ operator/opencl-tool.h
@@ -45,12 +45,7 @@ inline int my_get_load_type(size_t N)
   }
 }
 
-inline const string &make_kernel_name(const char *basic_name)
-{
-  static string name(basic_name);
-  cout << "Test make_kernel_name\n";
-  return name;
-}
+
 
 template <typename First, typename... _Args>
 const string &make_kernel_name(const char *basic_name)
@@ -58,5 +53,12 @@ const string &make_kernel_name(const char *basic_name)
   // 给basic_name拼接类型信息
   static string name = make_kernel_name<_Args...>(basic_name) + "_" + my_GetFullName<DType>();
   // 递归实现虽然会有不必要的静态变量，但是消耗的内存并不大
+  return name;
+}
+template<>
+const string &make_kernel_name<>(const char *basic_name)
+{
+  static string name(basic_name);
+  cout << "Test make_kernel_name\n";
   return name;
 }
